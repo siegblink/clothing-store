2004e38166fb68fb94547eefa08892640130ddfa
Siegfred Balona <siegblink@gmail.com>
2020-06-26 00:09:07 +0800

    Refactor sign-in and sign-up component.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './sign-in.styles.scss'
 
-class SignIn extends Component {
+export default class SignIn extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -10,17 +10,39 @@ class SignIn extends Component {
     }
   }
 
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({ email: '', password: '' })
+  }
+
+  handleChange = (event) => {
+    const { value, name } = event.target
+    this.setState({ [name]: value })
+  }
+
   render() {
     return (
       <div className='sign-in'>
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form action=''>
-          <input type='email' name='email' value={this.state.email} required />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='email'
+            name='email'
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
           <label htmlFor='email'>Email</label>
-          <input type='password' name='password' value={this.state.password} />
+          <input
+            type='password'
+            name='password'
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
           <label htmlFor='password'>Password</label>
+          <input type='submit' value='Submit form' />
         </form>
       </div>
     )
--- src/pages/sign-in-sign-up/sign-in-sign-up.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import SignIn from '../../components/sign-in/sign-in.component'
 import './sign-in-sign-up.scss'
 
 export default function SignInSignUp() {
-  return <div className='sign-in-sign-up'>Sign in</div>
+  return (
+    <div className='sign-in-sign-up'>
+      <SignIn />
+    </div>
+  )
 }
