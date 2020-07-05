5b356f2e4421c0cefffbcd7815e28a1a8b0769ee
Siegfred Balona <siegblink@gmail.com>
2020-07-06 00:38:37 +0800

    Refactor app component to use the createUserProfileDocument function.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up'
 import Header from './components/header/header.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import './App.css'
 
 class App extends React.Component {
@@ -18,9 +18,8 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user })
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
+      createUserProfileDocument(user)
     })
   }
 
