5ffe7b972c3f2111618dcc8ebe258b472fca92e3
Siegfred Balona <siegblink@gmail.com>
2020-07-07 23:59:46 +0800

    Setup firebase database snapshot side effect.

--- src/App.js
+++ src/App.js
@@ -18,8 +18,19 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: { id: snapshot.id, ...snapshot.data() },
+          })
+        })
+        console.log(this.state)
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
