1a890f7febc13ec0c56b0888099d516df4af7a74
Siegfred Balona <siegblink@gmail.com>
2020-07-09 23:41:21 +0800

    Add temporary console.log in app component.

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
           this.setState({
             currentUser: { id: snapshot.id, ...snapshot.data() },
           })
+          console.log(this.state)
         })
       } else {
         this.setState({ currentUser: userAuth })
