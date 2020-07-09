86e1f80e66cf2053d07e6fc1f2465d0981e59dec
Siegfred Balona <siegblink@gmail.com>
2020-07-09 23:43:24 +0800

    Include sign up component in sign-in-sign-up component.

--- src/pages/sign-in-sign-up/sign-in-sign-up.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 import './sign-in-sign-up.scss'
 
 export default function SignInSignUp() {
   return (
     <div className='sign-in-sign-up'>
       <SignIn />
+      <SignUp />
     </div>
   )
 }
